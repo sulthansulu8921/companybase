60dc8faea71a2397786d7ebfea9f347efb4ac08a
sulthan <sulthanshafeer713@gmail.com>
2026-08-15 08:08:05 +0530

    Remove ProductServices cards grid section from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 // src/pages/Home.jsx
 import React from "react";
 import Carousel from "../components/layout/Carousel";
-import ProductServices from "./Productservices";
 
 const Home = ({ setCurrentPage }) => {
   return (
     <div>
       <Carousel setCurrentPage={setCurrentPage} />
-      <ProductServices setCurrentPage={setCurrentPage} />
     </div>
   );
 };
